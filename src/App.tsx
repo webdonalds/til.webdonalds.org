7df15649d80d33a42c9c3ea40629668559cdbccc
Hoerin Doh <hellodhlyn@gmail.com>
2021-08-01 23:44:17 +0900

    Adding tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App bg-gray-000 min-h-screen">
+      <header className="w-full px-4 py-16 text-center">
+        <p className="text-5xl font-extrabold text-gray-900">Today I Learned</p>
       </header>
     </div>
   );
