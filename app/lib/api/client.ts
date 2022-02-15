f0bbb5e6f6dec036d5d6374d75494c83d402b52a
Hoerin Doh <hellodhlyn@gmail.com>
2022-02-15 22:54:04 +0900

    Add exchangeRequestPolicy for url client

--- app/lib/api/client.ts
+++ app/lib/api/client.ts
@@ -1,9 +1,16 @@
-import { createClient } from "@urql/core";
+import { cacheExchange, createClient, dedupExchange, fetchExchange } from "@urql/core";
+import { requestPolicyExchange } from "@urql/exchange-request-policy";
 
 const adminSecret = process.env.HASURA_ADMIN_SECRET;
-const headers = adminSecret ? { "X-Hasura-Admin-Secret": adminSecret } : {};
+const headers: Record<string, string> = adminSecret ? { "X-Hasura-Admin-Secret": adminSecret } : {};
 
 export const client = createClient({
   url: "https://engaging-mustang-19.hasura.app/v1/graphql",
   fetchOptions: { headers },
+  exchanges: [
+    dedupExchange,
+    requestPolicyExchange({ ttl: 60_000 }),
+    cacheExchange,
+    fetchExchange,
+  ],
 });
