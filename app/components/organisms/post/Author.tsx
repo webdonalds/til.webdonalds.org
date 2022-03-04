3f2984d5e6e1f8383dd6fab474f908ae321167a4
Hoerin Doh <hellodhlyn@gmail.com>
2022-03-04 23:19:39 +0900

    Refactor PostView to follow atomic design system

--- app/components/organisms/post/Author.tsx
+++ app/components/organisms/post/Author.tsx
@@ -1,40 +1,36 @@
 import { Link } from "remix";
-import dayjs from "dayjs";
 import { OuterLink } from "~/components/molecules/link";
+import { Author } from "~/models";
 
 type AuthorProps = {
-  id: number;
-  name: string;
-  profileUrl: string;
-  blogUrl?: string;
-  githubId?: string;
-  twitterId?: string;
-  instagramId?: string;
-  linkedInId?: string;
-  createdAt: Date;
+  data: Author;
 };
 
-export function Author(props: AuthorProps) {
-  const { id, name, profileUrl, blogUrl, githubId, twitterId, instagramId, linkedInId, createdAt } = props;
+export function Author({ data: author }: AuthorProps) {
+  const { id, name, profileImageUrl, social } = author;
+
+  const { blogUrl, githubId, twitterId, instagramId, linkedInId } = social;
+  const socialExists = !!(blogUrl || githubId || twitterId || instagramId || linkedInId);
+
   return (
     <div className="flex my-8 items-center">
-      <img className="h-8 w-8 md:h-12 md:w-12 rounded-full mr-3" src={profileUrl} alt={`${name}의 프로필 이미지`} />
+      <img className="h-12 w-12 rounded-full mr-3" src={profileImageUrl} alt={`${name}의 프로필 이미지`} />
       <div>
-        <p className="text-gray-900 dark:text-gray-100">
+        <p className="text-xl text-gray-900 dark:text-gray-100">
           by&nbsp;
           <Link className="hover:underline hover:opacity-75 transition" to={`/authors/${id}`}>
             <span className="font-bold">{name}</span>
           </Link>
         </p>
-        <p className="text-xs space-x-1">
-          <span>{dayjs(createdAt).format("YYYY. MM. DD.")}</span>
-          {(blogUrl || githubId || twitterId || instagramId || linkedInId) && <span className="md:px-1">|</span>}
-          {blogUrl && <OuterLink url={blogUrl}>블로그</OuterLink>}
-          {githubId && <OuterLink url={`https://github.com/${githubId}`}>깃허브</OuterLink>}
-          {linkedInId && <OuterLink url={`https://www.linkedin.com/in/${linkedInId}`}>링크드인</OuterLink>}
-          {twitterId && <OuterLink url={`https://twitter.com/${twitterId}`}>트위터</OuterLink>}
-          {instagramId && <OuterLink url={`https://instagram.com/${instagramId}`}>인스타그램</OuterLink>}
-        </p>
+        {socialExists &&
+          <p className="text-xs space-x-1">
+            {blogUrl && <OuterLink url={blogUrl}>블로그</OuterLink>}
+            {githubId && <OuterLink url={`https://github.com/${githubId}`}>깃허브</OuterLink>}
+            {linkedInId && <OuterLink url={`https://www.linkedin.com/in/${linkedInId}`}>링크드인</OuterLink>}
+            {twitterId && <OuterLink url={`https://twitter.com/${twitterId}`}>트위터</OuterLink>}
+            {instagramId && <OuterLink url={`https://instagram.com/${instagramId}`}>인스타그램</OuterLink>}
+          </p>
+        }
       </div>
     </div>
   );
--- app/routes/posts/$id.tsx
+++ app/routes/posts/$id.tsx
@@ -1,10 +1,10 @@
 import { json, LoaderFunction, MetaFunction, useCatch, useLoaderData } from "remix";
 import { DataFunctionArgs } from "@remix-run/server-runtime";
 import { gql } from "@urql/core";
-import { marked } from "marked";
 import { client } from "~/lib/api/client.server";
 import { ErrorMessage } from "~/components/templates/error";
-import { Author, Comment } from "~/components/organisms/post";
+import PostView from "~/components/templates/post-viewer";
+import { Post } from "~/models";
 
 type PostData = {
   til_posts: {
@@ -22,22 +22,6 @@ type PostData = {
     };
     created_at: string;
   }[];
-};
-
-type PostProps = {
-  title: string;
-  content: string;
-  author: {
-    id: number;
-    name: string;
-    profileUrl: string;
-    blogUrl: string | null;
-    githubId: string | null;
-    twitterId: string | null;
-    instagramId: string | null;
-    linkedInId: string | null;
-  };
-  createdAt: Date;
 };
 
 const query = gql<PostData>`
@@ -67,18 +51,20 @@ export const loader: LoaderFunction = async (args: DataFunctionArgs) => {
   }
 
   const post = data.til_posts[0];
-  return json<PostProps>({
+  return json<Post>({
     title: post.title,
     content: post.content,
     author: {
       id: post.author.id,
       name: post.author.display_name,
-      profileUrl: post.author.profile_image,
-      blogUrl: post.author.blog_url,
-      githubId: post.author.github_id,
-      twitterId: post.author.twitter_id,
-      instagramId: post.author.instagram_id,
-      linkedInId: post.author.linked_in_id,
+      profileImageUrl: post.author.profile_image,
+      social: {
+        blogUrl: post.author.blog_url,
+        githubId: post.author.github_id,
+        twitterId: post.author.twitter_id,
+        instagramId: post.author.instagram_id,
+        linkedInId: post.author.linked_in_id,
+      },
     },
     createdAt: new Date(post.created_at),
   });
@@ -113,30 +99,6 @@ export const meta: MetaFunction = ({ data, params }) => {
 };
 
 export default function Post() {
-  const { title, content, author, createdAt } = useLoaderData<PostProps>();
-  return (
-    <>
-      <Author
-        id={author.id}
-        name={author.name}
-        profileUrl={author.profileUrl}
-        blogUrl={author.blogUrl || undefined}
-        githubId={author.githubId || undefined}
-        twitterId={author.twitterId || undefined}
-        instagramId={author.instagramId || undefined}
-        linkedInId={author.linkedInId || undefined}
-        createdAt={createdAt}
-      />
-      <div className="my-8 px-6 md:px-8 py-8 bg-white dark:bg-gray-800 rounded-xl shadow-lg">
-        <div className="my-4 text-2xl md:text-3xl font-bold text-gray-900 dark:text-gray-100">
-          {title}
-        </div>
-        <article className="prose dark:prose-dark max-w-none break-words">
-          <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
-        </article>
-      </div>
-
-      <Comment className="my-8" />
-    </>
-  );
+  const post = useLoaderData<Post>();
+  return <PostView data={post} />;
 }
